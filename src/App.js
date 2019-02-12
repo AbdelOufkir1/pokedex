41fb61f045bac5b385e5b2d3a1c294f7c1ff478c
daniel ashley <danielashley@pursuit.org>
2019-02-11 22:16:05 -0500

    updated App.js with first 20 axios call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
+import  axios from 'axios';
 
 
 class App extends Component {
-  render() {
-    return (
-     <h1> Hello Pokedex Page </h1>
-    );
+  constructor(props){
+    super(props)
+    this.state = {
+       pokemon : [ 
+         {          name : 'pikachu',
+                    image: 'www.pika.com',
+                    number: '001',
+      }, ]
+    }
   }
+componentDidMount(){
+this.pagination()
+
+
 }
+
+
+  pagination(){
+     return axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20')
+     .then((response)=>{
+
+      // console.log(response)
+      this.setState({
+          pokemon : response.data.results,
+
+      })
+    console.log(this.state.pokemon)
+
+
+     })
+  }
+  
+    render() {
+      return (
+       <h1> Hello Pokedex Pagess </h1>
+      );
+    }
+  }
 
 export default App;
