b3fdd7fd9e4e6a6c461dda1699447382167de947
ositaigwe@pursuit.org <j0h@example.com>
2019-02-11 20:31:01 -0500

    adds components folder and header component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import Header from './components/header'
 
 class App extends Component {
   render() {
     return (
-     <h1> Hello Pokedex Page </h1>
+     <Header />
     );
   }
 }
