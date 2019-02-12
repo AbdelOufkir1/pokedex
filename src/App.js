e3fa73f42002cdcdb58b0c66c6da8e91ffaed25c
Abdel <abdelwahaboufkir@pursuit.org>
2019-02-12 14:51:03 -0500

    Added Daniel and Jennifer Axios update to the edited master + added the logic to display the 20 first pokemon in the list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
 import React, { Component } from 'react';
-import Header from './components/header'
+import axios from 'axios';
+import Header from './components/header';
+import Card from './components/card';
+import Button from './components/button'
+import Pokelist from './components/pokelist'
 
 class App extends Component {
+  constructor (props) {
+    super (props)
+
+    this.state ={
+      pokemon: [],
+    }
+  }
+  
+  pagination(){
+
+    return axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20')
+      .then((response)=>{
+      
+        const pokeArray = response.data.results;
+        const newArr = [];
+  
+        pokeArray.map( (e, idx)=>{
+  
+          newArr.push({
+                name: e.name, 
+                icon: `https://img.pokemondb.net/sprites/sun-moon/icon/${e.name}.png`,
+                id : String(idx)
+          })
+        })
+        this.setState({
+          pokemon: [].concat(newArr)
+        })
+      })
+  }
+  
+
+  componentDidMount() {
+    this.pagination();
+  }
+
   render() {
     return (
-     <Header />
+      <>
+      < Header />
+      <div className="container">
+      
+        {
+          this.state.pokemon.map((e,i) => {
+            return <Card key={i} pokeData={e} /> 
+          })
+        }            
+      <Button />
+      </div>     
+     </>
     );
   }
 }
--- src/components/header.js
+++ src/components/header.js
@@ -4,20 +4,19 @@ import './header.css'
 const Header = () => {
     return <>
         <div className="red"></div>
-        <div className="container-fluid">
+        <div className="container-fluid pokeHeader">
             <div className="row darkBorder">
                 <div className="col-4">
                     <img src="https://ih1.redbubble.net/image.250345047.6931/pp,550x550.u1.jpg" alt="Poke Ball" className="poke float-left" />
                 </div>
                 <div className="col-4 center-block">
                     <h1>Pursuit Pokedex</h1>
-                    <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" />
+                    <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" />
                 </div>
                 <div className="col-4">
                     <img src="https://ih1.redbubble.net/image.250345047.6931/pp,550x550.u1.jpg" alt="Poke Ball" className="poke float-right" />
                 </div>
-            </div>
-            <hr />
+            </div>  
         </div>
     </>
 }
