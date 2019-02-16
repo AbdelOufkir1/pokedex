45c5a596b3864deff19333230849b5b01f1955aa
daniel ashley <danielashley@pursuit.org>
2019-02-15 21:25:23 -0500

    Profile Data, and js logic

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import axios from 'axios';
 import Header from './components/header';
 import Card from './components/card';
 import Button from './components/button'
+import Profile from './components/profile'
 // import Pokelist from './components/pokelist'
 // import toProfile from './components/profile'
 
 
 class App extends Component {
-  constructor (props) {
-    super (props)
+  constructor(props) {
+    super(props)
 
     this.state ={
       pokemon: [],
+      view: false,
+      pokemonProfile : [{'name': 'name'}],
+      pokemonChosenIdx : 0,
     }
   }
 
@@ -75,19 +79,89 @@ class App extends Component {
   }
   
   toProfile=(name)=>{
-    console.log(name)
-    return axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
-    .then((response)=>{
+        const profileArr =[]
+        console.log(name)
+        return axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+            .then((response)=>{
+                  console.log('this is response',response)
+                  const pokemonData = response.data
+                  profileArr.push(
+                      { name: response.data.name,
+                        id:  response.data.id,
+
+                        profilePic : `https://img.pokemondb.net/artwork/${name}.jpg`,
+
+
+                      sprites:{
+                          back_default: pokemonData.sprites.back_default,
+                          back_shiny:pokemonData.sprites.back_shiny,
+                          front_default: pokemonData.sprites.front_default,
+                          front_shiny: pokemonData.sprites.front_shiny,
+                          },
+
+                      types : [pokemonData.types.map(e=>{
+                        return e.type.name
+                      })],
+
+                      stats : pokemonData.stats.map(e=>{
+                         return {
+                            statName : e.stat.name,
+                            baseStat : e.base_stat,
+
+                          }
+
+                      }),
+
+                      moves : pokemonData.moves.map(e=>{
+                        return {
+                          moveName : e.move.name,
+                          moveUrl : e.move.url,
+
+
+                        }
+
+                      })
+
+                      
+                  })
+                  
+                }).then(()=>{
+
+                    this.setState({
+                      view: !this.state.view,
+                      pokemonProfile: (this.state.pokemonProfile || []).concat(profileArr),
+                     
+                    }, ()=>{
+                    this.setState({
+                      pokemonChosenIdx : this.state.pokemonProfile.length -1
+
+
+                    })
+                    })
+
+                  })
+                }
+                  
+
+          // return profileArr
+            
+          
+              
+    
   
-  console.log(response)
-  
-    })
     
   
   
   
-  }
+    
+toggleMode=()=>{
 
+  console.log('aaaa')
+  this.setState({
+      view : !this.state.view,
+
+  })
+}
 
 
 
@@ -106,16 +180,38 @@ class App extends Component {
 
 
   render() {
+  console.log(this.state)
+        // const view1 = this.state.pokemon.map((e,i) => {
+        // return <Card key={i} pokeData={e} profile={this.toProfile} /> 
+
+        // })
+   
+        // const view2 = <Profile pokemonClicked={this.state.pokemonProfile[this.state.pokemonChosenIdx]}/>
+
+
+      
+    
     return (
       <>
       < Header />
       <div className="container">
       
-        {
+        {/* {  
+              
           this.state.pokemon.map((e,i) => {
             return <Card key={i} pokeData={e} profile={this.toProfile} /> 
-          })
-        }            
+    
+            })} */}
+
+         { this.state.view === false ? this.state.pokemon.map((e,i) => {
+            return <Card key={i} pokeData={e} profile={this.toProfile} /> 
+    
+            })  : <Profile pokemonClicked={this.state.pokemonProfile[this.state.pokemonChosenIdx]}/>
+        }       
+        {/* {this.state.view === 1 ?   :  this.state.pokemon.map((e,i) => {
+            return <Card key={i} pokeData={e} profile={this.toProfile} /> 
+    
+            }) } */}
       <Button loadMorePoke={this.loadMore}/>
       </div>     
      </>
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,47 +1,32 @@
 import React from 'react';
 import './profile.css';
 
+const Profile=(props)=>{
 
-
-class profile extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            pokemonProfile : [{
-                name: 'charmander',
-
-                sprites:{
-                back_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/4.png",
-                back_shiny: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/4.png",
-                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
-                front_shiny: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/4.png",
-
-                },
-
-                types : types.types.name
-
-
-                
-                
-               
-        }
-    }
-        
-
-render(){
-        return (
-         <h1>hello world</h1>
-
-
-
-                )
+console.log('img needed',props.pokemonClicked.profilePic)
+return (
+<div class="card text-center">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card-footer text-muted">
+    2 days ago
+  </div>
+</div>
+)
+    
+}
 
 
 
 
-        }
 
-    }
+export default Profile
 
 
 
