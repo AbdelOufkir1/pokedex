0607d0ce46057ad8814abae418e5b36432da270d
daniel ashley <danielashley@pursuit.org>
2019-02-16 21:04:37 -0500

    tags and moves update

--- src/components/modal.js
+++ src/components/modal.js
@@ -3,13 +3,9 @@ import './modal.css'
 
 
 const Modal = (props) => {
-    const showHideClassName = props.showState ? "modal display-block" : "modal display-none";
-  console.log('showState',props.showState)
     return (
-      <div className={showHideClassName}>
-        <section className="modal-main">
-          <button onClick={props.hideModal}>close</button>
-        </section>
+      <div>
+        
       </div>
     );
   };
--- src/components/profile.js
+++ src/components/profile.js
@@ -6,33 +6,23 @@ import Modal from './modal';
 const Profile=(props)=>{
 console.log('propsShow',props.showState)
 console.log('img needed',props.pokemonClicked.profilePic)
-return (
-<div className="card text-center">
-  <div className="card-header">
-    Featured
-  </div>
-  <div className="card-body">
-    <h5 className="card-title">Special title treatment</h5>
-    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-    <main>
-        <h1>React Modal</h1>
-        <Modal stateCheck={props.ShowState}>
-          <p>Modal</p>
-          <p>Data</p>
-        </Modal>
-        <button type="button" onClick={props.show}>
-          open
-        </button>
-      </main>
-    );
-  }
-  </div>
-  <div className="card-footer text-muted">
-    2 days ago
-  </div>
-</div>
-)
+console.log('right one', typeof props.pokemonClicked.moves)
+
+  return  <>
+           
+            <div >
+                {
+                    props.pokemonClicked.moves.map((e,i )=>{
+                        return <button className="scrolling-wrapper">{e[0]}</button>
+                    })
+                }
+            </div>
+           
+        </>
+    //  return <button onClick={()=><Modal />}>{props.pokemonClicked.name} </button>
+
+
+
     
 }
 
